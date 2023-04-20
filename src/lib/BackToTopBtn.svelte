9ae363f0d81d671aebb23f9c92df3079bcbaf820
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-20 14:51:23 -0700

    Don't hide button if were not scrolling up

--- src/lib/BackToTopBtn.svelte
+++ src/lib/BackToTopBtn.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
 	let windowScrollY: number
 	let windowHeight: number
-
-	let lastScrollY = 0
-	let isScrolledUp = false
-	const handleScroll = () => {
-		if (lastScrollY > windowScrollY) {
-			isScrolledUp = true
-		} else {
-			isScrolledUp = false
-		}
-		lastScrollY = windowScrollY
-	}
 </script>
 
-<svelte:window
-	on:scroll={handleScroll}
-	bind:scrollY={windowScrollY}
-	bind:innerHeight={windowHeight}
-/>
+<svelte:window bind:scrollY={windowScrollY} bind:innerHeight={windowHeight} />
 
 <a
 	class="btn fixed bottom-0 right-4 z-30 translate-y-full duration-200 xl:right-8
-	{windowScrollY >= windowHeight &&
-		isScrolledUp &&
-		'!-translate-y-4 xl:!-translate-y-8'}"
+	{windowScrollY >= windowHeight && '!-translate-y-4 xl:!-translate-y-8'}"
 	href="#body"
 >
 	Return to top
