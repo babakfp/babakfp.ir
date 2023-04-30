cc046627a60209a2cbdb45c33ed4d64a0dfc5e26
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-30 15:20:35 -0700

    Stuff

--- src/lib/markdown/blockquote.svelte
+++ src/lib/markdown/blockquote.svelte
@@ -4,7 +4,7 @@
 
 <blockquote>
 	<div
-		class="absolute left-0 top-0 flex h-7 w-7 -translate-x-2 -translate-y-2 items-center justify-center rounded-full bg-gray-700 text-sm text-gray-500"
+		class="absolute left-0 top-0 flex h-7 w-7 -translate-x-2.5 -translate-y-2 items-center justify-center rounded-full border-2 border-gray-50/5 bg-gray-800 text-sm text-gray-500"
 	>
 		<IconQuoteLeft />
 	</div>
