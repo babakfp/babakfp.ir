038b8751843c3fc51e2560d0d66f19a4338a2501
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-30 14:50:56 -0700

    Added markdown custom blockquote component

--- src/lib/markdown/MdsvexLayout.svelte
+++ src/lib/markdown/MdsvexLayout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" context="module">
 	import img from "./img.svelte"
-	export { img }
+	import blockquote from "./blockquote.svelte"
+	export { img, blockquote }
 </script>
 
 <slot />
---
+++ src/lib/markdown/blockquote.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { IconQuoteLeft } from "$icons"
+</script>
+
+<blockquote>
+	<div
+		class="absolute left-0 top-0 flex h-7 w-7 -translate-x-2 -translate-y-2 items-center justify-center rounded-full bg-gray-700 text-sm text-gray-500"
+	>
+		<IconQuoteLeft />
+	</div>
+
+	<slot />
+</blockquote>
