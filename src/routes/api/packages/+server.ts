e9614d45976f20e165ad5ceadf5c097b1b86f38b
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-30 19:08:56 -0700

    Fixed prerender

--- src/routes/api/packages/+server.ts
+++ src/routes/api/packages/+server.ts
@@ -1,6 +1,8 @@
 import { json } from "@sveltejs/kit"
 import { getPackagesData } from "$utils/fetchNpmPackages/index"
 
+export const prerender = false
+
 export async function GET() {
 	const packagesData = await getPackagesData()
 	return json(packagesData)
