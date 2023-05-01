317dabd6048a44408a46edece7dfba3baa477300
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-30 19:10:30 -0700

    Fixed typo

--- src/routes/api/packages/+server.ts
+++ src/routes/api/packages/+server.ts
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit"
 import { getPackagesData } from "$utils/fetchNpmPackages/index"
 
-export const prerender = false
+export const prerender = true
 
 export async function GET() {
 	const packagesData = await getPackagesData()
