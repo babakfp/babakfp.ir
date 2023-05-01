30dc0e55eb3bee0d778cfe934def9f8d4bd9b7cb
babakfp <44144724+babakfp@users.noreply.github.com>
2023-05-01 11:12:57 -0700

    Added rehype-autolink-headings

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,8 @@ import { getHighlighter } from "shiki"
 import remarkUnwrapImages from "remark-unwrap-images"
 import rehypeSlug from "rehype-slug"
 import rehypeExternalLinks from "rehype-external-links"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import { s } from "hastscript"
 // import { rehypeAccessibleEmojis } from "rehype-accessible-emojis"
 
 const mdsvexExtension = ".md"
@@ -39,6 +41,38 @@ export default {
 								: undefined
 						},
 						rel: ["nofollow", "noopener", "noreferrer"],
+					},
+				],
+				[
+					rehypeAutolinkHeadings,
+					{
+						properties: {
+							class: "headings-permalink",
+							"aria-label": "Permalink to this headline",
+						},
+						content(node) {
+							return [
+								// <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5"/></svg>
+								s(
+									"svg",
+									{
+										xmlns: "http://www.w3.org/2000/svg",
+										fill: "none",
+										stroke: "currentColor",
+										"stroke-width": "1.5",
+										viewBox: "0 0 24 24",
+									},
+									[
+										s("path", {
+											"stroke-linecap": "round",
+											"stroke-linejoin": "round",
+											d: "M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5",
+										}),
+									]
+								),
+							]
+						},
+						test: ["h2", "h3", "h4", "h5", "h6"],
 					},
 				],
 				// rehypeAccessibleEmojis,
