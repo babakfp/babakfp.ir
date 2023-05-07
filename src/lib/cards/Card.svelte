4758587a16334b032a2299e46f62708b7e2a648a
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-07 10:13:32 -0700

    Formatted

--- src/lib/cards/Card.svelte
+++ src/lib/cards/Card.svelte
@@ -16,7 +16,7 @@
 
 	{#if isExternal}
 		<div
-			class="absolute right-0 top-0 flex -translate-x-4 translate-y-4 text-gray-400 sm:-translate-y-full sm:translate-x-full sm:scale-0 sm:duration-200 sm:group-hover:translate-y-4 sm:group-hover:-translate-x-4 sm:group-hover:scale-100"
+			class="absolute right-0 top-0 flex -translate-x-4 translate-y-4 text-gray-400 sm:-translate-y-full sm:translate-x-full sm:scale-0 sm:duration-200 sm:group-hover:-translate-x-4 sm:group-hover:translate-y-4 sm:group-hover:scale-100"
 		>
 			<IconArrowUpRightSquare />
 		</div>
