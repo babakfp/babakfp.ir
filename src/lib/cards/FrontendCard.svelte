a2e1af62456039ba1942a0cb1f0a7c690696489a
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-18 17:13:25 -0700

    Changed frontend card transition

--- src/lib/cards/FrontendCard.svelte
+++ src/lib/cards/FrontendCard.svelte
@@ -16,16 +16,18 @@
     rel="noreferrer"
 >
     <div class="relative {isSingleRow && 'lg:col-start-1 lg:col-end-8'}">
-        <img
-            class="aspect-[16/9] w-full rounded-[2px] bg-gray-800 duration-200 group-hover:scale-95"
-            src={poster}
-            alt="Portfolio poster"
-        />
+        <div class="relative overflow-hidden">
+            <img
+                class="aspect-[16/9] w-full rounded-[2px] bg-gray-800 duration-700 ease-in-out group-hover:scale-95"
+                src={poster}
+                alt="Portfolio poster"
+            />
+            <div
+                class="absolute inset-0 aspect-[16/9] h-full w-full -translate-y-8 scale-90 rounded-[2px] border-2 border-dashed border-gray-50/20 duration-1000 ease-in-out hide group-hover:translate-y-0 group-hover:rotate-1 group-hover:scale-[.85] group-hover:show"
+            />
+        </div>
         <div
-            class="absolute inset-0 -z-1 aspect-[16/9] h-full w-full rounded-[2px] bg-gray-50/10 duration-200 hide group-hover:-translate-y-4 group-hover:show"
-        />
-        <div
-            class="absolute inset-0 aspect-[16/9] h-full w-full -translate-y-8 scale-90 rounded-[2px] border-2 border-dashed border-gray-50/20 duration-500 hide group-hover:translate-y-0 group-hover:rotate-1 group-hover:scale-[.85] group-hover:show"
+            class="absolute inset-0 -z-1 aspect-[16/9] h-full w-full rounded-[2px] bg-gray-50/10 duration-700 ease-in-out hide group-hover:-translate-y-4 group-hover:show"
         />
     </div>
     <div
