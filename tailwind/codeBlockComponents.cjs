cbe9720546b3024e2e99fcc5598a36c307ef494f
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-24 18:42:03 -0700

    Stuff

--- tailwind/codeBlockComponents.cjs
+++ tailwind/codeBlockComponents.cjs
@@ -3,13 +3,13 @@ module.exports =
     ({ addComponents }) => {
         // prettier-ignore
         addComponents({
-			".pre-code-block": {
+			".code-block": {
 				"@apply rounded !bg-gray-50/5 p-0.5 text-sm [tab-size:4] highlight-gray-50/10 selection:bg-indigo-400/25": "",
 			},
-			".pre-code-block, .pre-code-block *": {
+			".code-block, .code-block *": {
 				"@apply !not-italic select-text": "",
 			},
-			".pre-code-block code": {
+			".code-block code": {
 				"@apply block overflow-x-auto select-text p-6 leading-6": "",
 			},
 			".inline-code": {
