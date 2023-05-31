f5f0e19c566d4b97b959d667436e101aa60d8252
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 11:44:15 -0700

    Fetch all posts in post single page

--- src/routes/(app)/blog/[slug]/+page.ts
+++ src/routes/(app)/blog/[slug]/+page.ts
@@ -1,9 +1,14 @@
 import { error } from "@sveltejs/kit"
+import type { Post } from "$lib/types.js"
 
-export async function load({ params }) {
+export async function load({ fetch, params }) {
+    const res = await fetch("/api/posts")
+    const posts: Post[] = await res.json()
+
     try {
         const post = await import(`../../../../posts/${params.slug}/+page.md`)
         return {
+            posts,
             ...post.metadata,
             content: post.default,
         }
