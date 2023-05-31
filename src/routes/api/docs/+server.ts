70e6583987985e4b3291ca4fdef4df7011fc0c8c
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 14:49:53 -0700

    Added docs api

---
+++ src/routes/api/docs/+server.ts
@@ -0,0 +1,25 @@
+import { json } from "@sveltejs/kit"
+import type { Doc } from "$lib/types"
+
+export async function GET() {
+    const docs = await getDocs()
+    return json(docs)
+}
+
+async function getDocs() {
+    let docs: Doc[] = []
+
+    const paths = import.meta.glob("/src/docs/**/+page.md", { eager: true })
+
+    for (const path in paths) {
+        const file = paths[path]
+        const slug = path.split("/").at(-2)
+        if (file && typeof file === "object" && "metadata" in file && slug) {
+            const metadata = file.metadata as Omit<Doc, "slug">
+            const doc = { ...metadata, slug } satisfies Doc
+            docs.push(doc)
+        }
+    }
+
+    return docs
+}
