c23514a907d58c48065c6ce76639afbd04d02380
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 14:03:34 -0700

    Fixed warnings

--- src/lib/markdown/MdsvexLayout.svelte
+++ src/lib/markdown/MdsvexLayout.svelte
@@ -4,4 +4,12 @@
     export { img, blockquote }
 </script>
 
+<script lang="ts">
+    // Prevent warnings (<MdsvexLayout> was created with unknown prop 'title')
+    export let title = ""
+    export let description = ""
+    export let updated = ""
+    export let published = ""
+</script>
+
 <slot />
