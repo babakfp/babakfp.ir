f05f0312aaee4c238100792d3b13211180db5c18
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 11:50:25 -0700

    Fixed other blog posts

--- src/routes/(app)/blog/[slug]/+page.svelte
+++ src/routes/(app)/blog/[slug]/+page.svelte
@@ -69,14 +69,14 @@
                 </article>
             {/key}
 
-            {#if data.posts.length > 0}
+            {#if otherPosts.length > 0}
                 <hr class="my-8" />
 
                 <div class="[zoom:0.8]">
                     <h2 class="font-bold">OTHER BLOG POSTS</h2>
 
                     <ul class="mt-4 space-y-4">
-                        {#each data.posts as post}
+                        {#each otherPosts as post}
                             <li>
                                 <PostCard {...post} />
                             </li>
