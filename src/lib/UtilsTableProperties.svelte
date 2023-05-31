e1e91507d42d0b4315fa4f8e1937d72ad5490df0
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 16:21:10 -0700

    Better code block line height

--- src/lib/UtilsTableProperties.svelte
+++ src/lib/UtilsTableProperties.svelte
@@ -3,40 +3,42 @@
     export let indent = 16
 </script>
 
-{#each Object.entries(selectorAndStyles) as [propertyOrSelector, valueOrObjectOfPropertyAndValue]}
-    <!-- For variants -->
-    {#if valueOrObjectOfPropertyAndValue === ""}
-        <!-- selector -->
-        <div>{propertyOrSelector}</div>
-    {:else if typeof valueOrObjectOfPropertyAndValue === "string"}
-        <!-- property: value -->
-        <div>
-            <!-- prettier-ignore -->
-            <span style="color: #9CCFD8">{propertyOrSelector}<span style="color: #908CAA">:</span></span>
-            <!-- prettier-ignore -->
-            <span style="color: {Number.isNaN(valueOrObjectOfPropertyAndValue) ? '#F6C177' : '#EA9A97'}">{valueOrObjectOfPropertyAndValue}<span style="color: #908CAA">;</span></span>
-        </div>
-    {:else if typeof valueOrObjectOfPropertyAndValue === "object"}
-        <!-- {} -->
-        <!-- Spliting every selector into a new line
+<div class="leading-6">
+    {#each Object.entries(selectorAndStyles) as [propertyOrSelector, valueOrObjectOfPropertyAndValue]}
+        <!-- For variants -->
+        {#if valueOrObjectOfPropertyAndValue === ""}
+            <!-- selector -->
+            <div>{propertyOrSelector}</div>
+        {:else if typeof valueOrObjectOfPropertyAndValue === "string"}
+            <!-- property: value -->
+            <div>
+                <!-- prettier-ignore -->
+                <span style="color: #9CCFD8">{propertyOrSelector}<span style="color: #908CAA">:</span></span>
+                <!-- prettier-ignore -->
+                <span style="color: {Number.isNaN(valueOrObjectOfPropertyAndValue) ? '#F6C177' : '#EA9A97'}">{valueOrObjectOfPropertyAndValue}<span style="color: #908CAA">;</span></span>
+            </div>
+        {:else if typeof valueOrObjectOfPropertyAndValue === "object"}
+            <!-- {} -->
+            <!-- Spliting every selector into a new line
 		Used span to keep selector and { in the same line -->
-        {@const selectors = propertyOrSelector.split(",")}
-        {#each selectors as selector, i}
-            {@const isNotTheLastSelector = i < selectors.length - 1}
-            <svelte:element
-                this={isNotTheLastSelector ? "div" : "span"}
-                style="color: #908CAA"
-            >
-                {selector}{isNotTheLastSelector ? "," : ""}
-            </svelte:element>
-        {/each}
-        <span style="color: #908CAA">{"{"}</span>
-        <div style="text-indent: {indent}px">
-            <svelte:self
-                selectorAndStyles={valueOrObjectOfPropertyAndValue}
-                indent={(indent += 16)}
-            />
-        </div>
-        <span style="color: #908CAA">{"}"}</span>
-    {/if}
-{/each}
+            {@const selectors = propertyOrSelector.split(",")}
+            {#each selectors as selector, i}
+                {@const isNotTheLastSelector = i < selectors.length - 1}
+                <svelte:element
+                    this={isNotTheLastSelector ? "div" : "span"}
+                    style="color: #908CAA"
+                >
+                    {selector}{isNotTheLastSelector ? "," : ""}
+                </svelte:element>
+            {/each}
+            <span style="color: #908CAA">{"{"}</span>
+            <div style="text-indent: {indent}px">
+                <svelte:self
+                    selectorAndStyles={valueOrObjectOfPropertyAndValue}
+                    indent={(indent += 16)}
+                />
+            </div>
+            <span style="color: #908CAA">{"}"}</span>
+        {/if}
+    {/each}
+</div>
