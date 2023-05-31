8134d022d60774d2508ec1a5fa6201c63155aae0
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 11:39:07 -0700

    Removed OtherBlogPosts

--- src/routes/(app)/blog/(single)/+layout@.svelte
+++ src/routes/(app)/blog/(single)/+layout@.svelte
@@ -6,12 +6,16 @@
     import posts from "$portfolios/posts.json"
     import { isBlogTocSidebarOpen } from "$stores/blog"
     import { IconChevronRight } from "$icons"
-    import OtherBlogPosts from "./OtherBlogPosts.svelte"
     import BackToTopBtn from "$lib/BackToTopBtn.svelte"
     import TocMenu from "$lib/Sidebar/TocMenu.svelte"
+    import PostCard from "$lib/cards/PostCard.svelte"
 
     // prettier-ignore
     const {title, description, updateDate, publishDate} = posts.filter(({ slug }) => slug === $page.url.pathname.replace("/blog/", ""))[0]
+
+    const otherPosts = posts.filter(
+        ({ slug }) => slug !== $page.url.pathname.replace("/blog/", "")
+    )
 </script>
 
 <svelte:head>
@@ -65,7 +69,21 @@
                 </article>
             {/key}
 
-            <OtherBlogPosts />
+            {#if posts.length > 0}
+                <hr class="my-8" />
+
+                <div class="[zoom:0.8]">
+                    <h2 class="font-bold">OTHER BLOG POSTS</h2>
+
+                    <ul class="mt-4 space-y-4">
+                        {#each otherPosts as { slug, ...rest }}
+                            <li>
+                                <PostCard {...rest} href="/blog/{slug}" />
+                            </li>
+                        {/each}
+                    </ul>
+                </div>
+            {/if}
         </div>
 
         <TocMenu name="blog" bind:isOpen={$isBlogTocSidebarOpen} />
