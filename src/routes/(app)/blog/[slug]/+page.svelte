aa7c63e724fbadba7359073c413ec9dbc32944bf
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 11:51:11 -0700

    Renamed

--- src/routes/(app)/blog/[slug]/+page.svelte
+++ src/routes/(app)/blog/[slug]/+page.svelte
@@ -11,7 +11,7 @@
 
     export let data
 
-    const otherPosts = data.posts.filter(
+    const recommendedPosts = data.posts.filter(
         ({ slug }) => slug !== $page.url.pathname.replace("/blog/", "")
     )
 </script>
@@ -69,14 +69,14 @@
                 </article>
             {/key}
 
-            {#if otherPosts.length > 0}
+            {#if recommendedPosts.length > 0}
                 <hr class="my-8" />
 
                 <div class="[zoom:0.8]">
                     <h2 class="font-bold">OTHER BLOG POSTS</h2>
 
                     <ul class="mt-4 space-y-4">
-                        {#each otherPosts as post}
+                        {#each recommendedPosts as post}
                             <li>
                                 <PostCard {...post} />
                             </li>
