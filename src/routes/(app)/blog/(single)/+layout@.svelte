c1816b1d6ef0ae9ab1c6c5f6da40fb139c27d22d
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 11:38:07 -0700

    Removed Article

--- src/routes/(app)/blog/(single)/+layout@.svelte
+++ src/routes/(app)/blog/(single)/+layout@.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+    import { fly } from "svelte/transition"
     import { page } from "$app/stores"
+    import transition from "$utils/transition"
+    import { timeSince } from "$utils/timeSince"
     import posts from "$portfolios/posts.json"
     import { isBlogTocSidebarOpen } from "$stores/blog"
-
-    import Article from "./Article.svelte"
+    import { IconChevronRight } from "$icons"
     import OtherBlogPosts from "./OtherBlogPosts.svelte"
     import BackToTopBtn from "$lib/BackToTopBtn.svelte"
     import TocMenu from "$lib/Sidebar/TocMenu.svelte"
@@ -28,9 +30,40 @@
         <div
             class="min-w-0 xl:mr-4 xl:border-r xl:border-gray-50/5 xl:pb-[--main-spacing-b] xl:pr-12 xl:pt-[--main-spacing-t]"
         >
-            <Article {title} {updateDate} {publishDate}>
-                <slot />
-            </Article>
+            {#key $page.url.pathname}
+                <article in:fly={transition}>
+                    <ul class="flex items-center gap-2 text-sm">
+                        <li>
+                            <a class="link" href="/">HOME</a>
+                        </li>
+                        <IconChevronRight class="text-xs opacity-40" />
+                        <li>
+                            <a class="link" href="/blog">BLOG</a>
+                        </li>
+                    </ul>
+
+                    <div
+                        class="mt-4 flex flex-wrap items-center gap-x-4 gap-y-2 text-xs text-gray-400"
+                    >
+                        <span>
+                            Updated: <b title={updateDate}>
+                                {timeSince(new Date(updateDate))}
+                            </b>
+                        </span>
+                        <span>
+                            Published: <b title={publishDate}>
+                                {timeSince(new Date(publishDate))}
+                            </b>
+                        </span>
+                    </div>
+
+                    <div class="article-content">
+                        <h1 class="mt-8">{title}</h1>
+
+                        <slot />
+                    </div>
+                </article>
+            {/key}
 
             <OtherBlogPosts />
         </div>
