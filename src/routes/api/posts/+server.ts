32476dd7447368eec7ebc30a990c3b0f0cc151b0
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 11:13:01 -0700

    Fix ts type stuff

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -14,9 +14,11 @@ async function getPosts() {
     for (const path in paths) {
         const file = paths[path]
         const slug = path.split("/").at(-2)
-        const metadata = file.metadata
-        const post = { ...metadata, slug }
-        posts.push(post)
+        if (file && typeof file === "object" && "metadata" in file && slug) {
+            const metadata = file.metadata as Omit<Post, "slug">
+            const post = { ...metadata, slug } satisfies Post
+            posts.push(post)
+        }
     }
 
     posts = posts.sort(
