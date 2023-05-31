35994882200577b1789c8d777730a16ff10b529b
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-31 15:38:52 -0700

    Removed unused

--- src/routes/(app)/docs/[...slug]/+page.ts
+++ src/routes/(app)/docs/[...slug]/+page.ts
@@ -1,17 +1,12 @@
 import { error } from "@sveltejs/kit"
-import type { Doc } from "$lib/types.js"
 
-export async function load({ fetch, params }) {
-    const res = await fetch("/api/docs")
-    const docs: Doc[] = await res.json()
-
+export async function load({ params }) {
     try {
         const doc = await import(`../../../../docs/${params.slug}/+page.md`)
         return {
             ...doc.metadata,
             slug: params.slug,
             content: doc.default,
-            docs,
         }
     } catch (e) {
         return error(404)
