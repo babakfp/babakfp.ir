10b80681f17afa4877eb21c755871af5a5a9ac7d
Babak Farkhoopak <babak.bxf@gmail.com>
2023-06-12 11:02:47 -0700

    Fix

--- src/lib/markdown/PlatformQuote.svelte
+++ src/lib/markdown/PlatformQuote.svelte
@@ -4,7 +4,7 @@
 
 <div class="relative rounded border-4 border-gray-50/5 bg-gray-50/5 p-4 sm:p-8">
     <div
-        class="flex justify-center rounded-full bg-gray-700 px-container-x py-2 text-xs uppercase sm:-m-4"
+        class="flex justify-center rounded-full bg-gray-700 px-container-x py-2 text-xs uppercase"
     >
         {title}
     </div>
