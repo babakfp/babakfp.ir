a6ea5b528d95b2b7cc230953234b73cc55c73a1b
Babak Farkhoopak <babak.bxf@gmail.com>
2023-06-13 08:59:43 -0700

    Fix

--- src/lib/cards/FrontendCard.svelte
+++ src/lib/cards/FrontendCard.svelte
@@ -6,52 +6,53 @@
     export let isSingleRow: boolean | undefined = false
 </script>
 
-<a
-    class="
-		group block outline-offset-8 tap-highlight-transparent
-		{isSingleRow && 'lg:col-span-full lg:grid lg:grid-cols-12 lg:gap-6'}
-	"
-    href={url}
-    target="_blank"
-    rel="noreferrer"
->
-    <div class="relative {isSingleRow && 'lg:col-start-1 lg:col-end-8'}">
-        <div class="relative overflow-hidden">
-            <img
-                class="aspect-[16/9] w-full rounded-[2px] bg-gray-800 duration-700 ease-in-out group-hover:scale-95"
-                src={poster}
-                alt="Portfolio poster"
-            />
+<li class={isSingleRow ? "lg:col-span-full" : ""}>
+    <a
+        class="group block outline-offset-8 tap-highlight-transparent
+			{isSingleRow && 'lg:grid lg:grid-cols-12 lg:gap-6'}
+		"
+        href={url}
+        target="_blank"
+        rel="noreferrer"
+    >
+        <div class="relative {isSingleRow && 'lg:col-start-1 lg:col-end-8'}">
+            <div class="relative overflow-hidden">
+                <img
+                    class="aspect-[16/9] w-full rounded-[2px] bg-gray-800 duration-700 ease-in-out group-hover:scale-95"
+                    src={poster}
+                    alt="Portfolio poster"
+                />
+                <div
+                    class="absolute inset-0 aspect-[16/9] h-full w-full -translate-y-8 scale-90 rounded-[2px] border-2 border-dashed border-gray-50/20 duration-1000 ease-in-out hide group-hover:translate-y-0 group-hover:rotate-1 group-hover:scale-[.85] group-hover:show"
+                />
+            </div>
             <div
-                class="absolute inset-0 aspect-[16/9] h-full w-full -translate-y-8 scale-90 rounded-[2px] border-2 border-dashed border-gray-50/20 duration-1000 ease-in-out hide group-hover:translate-y-0 group-hover:rotate-1 group-hover:scale-[.85] group-hover:show"
+                class="absolute inset-0 -z-1 aspect-[16/9] h-full w-full rounded-[2px] bg-gray-50/10 duration-700 ease-in-out hide group-hover:-translate-y-4 group-hover:show"
             />
         </div>
         <div
-            class="absolute inset-0 -z-1 aspect-[16/9] h-full w-full rounded-[2px] bg-gray-50/10 duration-700 ease-in-out hide group-hover:-translate-y-4 group-hover:show"
-        />
-    </div>
-    <div
-        class="
+            class="
 			mt-4 grid content-center gap-2
 			{isSingleRow && 'lg:col-start-8 lg:col-end-13 lg:mt-0 lg:gap-4'}
 		"
-    >
-        <h2
-            class="text-xl font-bold text-gray-50
+        >
+            <h2
+                class="text-xl font-bold text-gray-50
 				{isSingleRow && 'lg:!text-3xl'}"
-        >
-            {title}
-        </h2>
-        <p
-            class="text-sm text-gray-400
+            >
+                {title}
+            </h2>
+            <p
+                class="text-sm text-gray-400
 				{isSingleRow && 'lg:text-lg'}"
-        >
-            {description}
-        </p>
-        {#if isSingleRow}
-            <span class="hidden lg:inline-block lg:text-sm lg:underline">
-                View Website
-            </span>
-        {/if}
-    </div>
-</a>
+            >
+                {description}
+            </p>
+            {#if isSingleRow}
+                <span class="hidden lg:inline-block lg:text-sm lg:underline">
+                    View Website
+                </span>
+            {/if}
+        </div>
+    </a>
+</li>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -19,9 +19,7 @@
 <section class="mt-14">
     <ul class="grid gap-8 sm:px-12 lg:grid-cols-2 lg:gap-6 lg:gap-y-8 xl:px-0">
         {#each frontendPortfolios as portfolio}
-            <li>
-                <FrontendCard {...portfolio} />
-            </li>
+            <FrontendCard {...portfolio} />
         {/each}
     </ul>
 </section>
