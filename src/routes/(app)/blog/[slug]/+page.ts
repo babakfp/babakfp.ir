1571aca9b3a487676de98019f4f6c3c2433d7040
babakfp <babak.bxf@gmail.com>
2023-12-19 12:11:34 -0800

    Used import.meta.glob instead of import for post single page

--- src/routes/(app)/blog/[slug]/+page.ts
+++ src/routes/(app)/blog/[slug]/+page.ts
@@ -4,16 +4,18 @@ import type { Post } from "$lib/types.js"
 export async function load({ fetch, params }) {
     const res = await fetch("/api/posts")
     const posts: Post[] = await res.json()
+    const paths = import.meta.glob("/src/posts/**/+page.md")
+    const path = paths[`/src/posts/${params.slug}/+page.md`]
 
-    try {
-        const post = await import(`../../../../posts/${params.slug}/+page.md`)
+    if (path) {
+        const post = await path()
         return {
             ...post.metadata,
             slug: params.slug,
             content: post.default,
             posts,
         }
-    } catch {
-        return error(404)
     }
+
+    error(404)
 }
