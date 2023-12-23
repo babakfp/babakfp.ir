bd68924253bdd3774ad1f8e845efa43be0da3786
babakfp <babak.bxf@gmail.com>
2023-12-22 17:26:03 -0800

    Remove hover styles on touch devices

---
+++ src/lib/utils/removeHoverOnTouch.ts
@@ -0,0 +1,27 @@
+export const removeHoverOnTouch = () => {
+    function hasTouch() {
+        return (
+            "ontouchstart" in document.documentElement ||
+            navigator.maxTouchPoints > 0
+        )
+    }
+
+    if (hasTouch()) {
+        // remove all the :hover stylesheets
+        try {
+            // prevent exception on browsers not supporting DOM styleSheets properly
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si]
+                if (!styleSheet.cssRules) continue
+
+                for (var ri = styleSheet.cssRules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.cssRules[ri].cssText) continue
+
+                    if (styleSheet.cssRules[ri].cssText.match(":hover")) {
+                        styleSheet.deleteRule(ri)
+                    }
+                }
+            }
+        } catch {}
+    }
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     import "../app.postcss"
     import { LoadingBar } from "svelte-loading-bar"
     import Header from "$lib/header/Header.svelte"
+    import { onMount } from "svelte"
+    import { removeHoverOnTouch } from "$utils/removeHoverOnTouch"
+
+    onMount(() => removeHoverOnTouch())
 </script>
 
 <LoadingBar />
