9b61290fdcfff8b84a91e8dddfb3601a25e1e7f1
babakfp <babak.bxf@gmail.com>
2024-03-06 13:47:09 -0800

    Added `getUtilities()`

---
+++ src/lib/utils/tailwind/getUtilities.ts
@@ -0,0 +1,50 @@
+import type { CSSRuleObject, PluginAPI } from "tailwindcss/types/config"
+import { default as twPlugin } from "tailwindcss/plugin"
+
+type RecursiveStringObject = {
+    [key: string]: string | RecursiveStringObject
+}
+
+export const getUtilities = (plugin: ReturnType<typeof twPlugin>) => {
+    const outputUtilities: RecursiveStringObject = {}
+
+    const addUtilities: PluginAPI["addUtilities"] = utilities => {
+        utilities = Array.isArray(utilities) ? utilities : [utilities]
+
+        utilities.forEach(utility => {
+            const cleanUtility = cleanCSSRuleObject(utility)
+
+            for (let [selector, properties] of Object.entries(cleanUtility)) {
+                for (const [property, value] of Object.entries(properties)) {
+                    if (property.startsWith("@defaults")) continue
+                }
+
+                outputUtilities[selector] = properties
+            }
+        })
+    }
+
+    plugin.handler({ addUtilities })
+
+    return outputUtilities
+}
+
+function cleanCSSRuleObject(utilities: CSSRuleObject) {
+    const recursiveStringObject: RecursiveStringObject = {}
+
+    for (const [key, value] of Object.entries(utilities)) {
+        if (typeof value === "string") {
+            recursiveStringObject[key] = value
+        } else if (
+            typeof value === "object" &&
+            !Array.isArray(value) &&
+            value !== null
+        ) {
+            recursiveStringObject[key] = cleanCSSRuleObject(value)
+        } else {
+            throw new Error("Type `RecursiveKeyValuePair` is only handled!")
+        }
+    }
+
+    return recursiveStringObject
+}
