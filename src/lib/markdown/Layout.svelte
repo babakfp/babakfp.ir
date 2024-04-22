e95ec524263aed153c4287d8365804c02faa9a15
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-04-22 22:10:42 +0330

    Located files

--- src/lib/markdown/Layout.svelte
+++ src/lib/markdown/Layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" context="module">
-    import img from "$lib/markdown/img.svelte"
-    import blockquote from "$lib/markdown/blockquote.svelte"
+    import img from "$lib/markdown/elements/img.svelte"
+    import blockquote from "$lib/markdown/elements/blockquote.svelte"
 
     export const markdownElements = { img, blockquote }
 </script>
