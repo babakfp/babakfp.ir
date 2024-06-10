f01441c6c2f0fabb7f43cbd56f7725ed6e1e18cb
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-06-10 07:05:50 +0330

    Styles

--- src/routes/(app)/portfolios/[slug]/TechnologyBox.svelte
+++ src/routes/(app)/portfolios/[slug]/TechnologyBox.svelte
@@ -2,9 +2,6 @@
     export let title: string
 </script>
 
-<div
-    class="flex rounded bg-gray-800 p-2 text-xl sm:p-3 sm:text-2xl lg:p-4 lg:text-3xl"
-    {title}
->
+<div class="flex rounded bg-gray-800 p-4 text-3xl" {title}>
     <slot />
 </div>
