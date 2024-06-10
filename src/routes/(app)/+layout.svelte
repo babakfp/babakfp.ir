16df6fc3e55045ca0c0fca6119bb23e05ae569b3
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-06-10 08:34:05 +0330

    Added Favicon by link in the footer

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -9,3 +9,10 @@
         <slot />
     </main>
 {/key}
+
+<div class="border-t border-gray-800 py-4 text-center">
+    <p class="text-sm">
+        Favicon by
+        <a class="link" href="https://icons8.com" target="_blank">Icon8</a>.
+    </p>
+</div>
