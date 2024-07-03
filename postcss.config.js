1cad5e0e7204269254b6519712c18536475ff760
babakfp <babak.bxf@gmail.com>
2024-07-03 21:20:14 +0330

    Stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
-const isProduction = process.env.NODE_ENV === "production"
-
 export default {
     plugins: {
         "tailwindcss/nesting": {},
         tailwindcss: {},
         autoprefixer: {},
-        ...(isProduction ? { cssnano: {} } : {}),
+        ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
     },
 }
