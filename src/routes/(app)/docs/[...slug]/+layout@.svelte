77e8cfda974d1cf48bb7d1361a9e8e0d188a842b
babakfp <babak.bxf@gmail.com>
2024-07-19 11:46:37 +0330

    Relocated

--- src/routes/(app)/docs/[...slug]/+layout@.svelte
+++ src/routes/(app)/docs/[...slug]/+layout@.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import BackToTopBtn from "$lib/BackToTopBtn.svelte"
     import DocsMenu from "$lib/docs/DocsMenu.svelte"
-    import Layout from "$lib/markdown/Layout.svelte"
+    import Layout from "$lib/markdown/layouts/Layout.svelte"
     import TocMenu from "$lib/Sidebar/TocMenu.svelte"
     import { isDocsTocSidebarOpen } from "$lib/stores/docs"
 </script>
--- src/routes/(app)/portfolios/[slug]/+layout.svelte
+++ src/routes/(app)/portfolios/[slug]/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Layout from "$lib/markdown/Layout.svelte"
+    import Layout from "$lib/markdown/layouts/Layout.svelte"
 </script>
 
 <Layout>
