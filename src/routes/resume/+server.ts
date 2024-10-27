53a2cad79a67498353147e14bd78109bb3835a8e
babakfp <babak.bxf@gmail.com>
2024-10-27 10:28:28 +0330

    Fixed /resume

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit"
 import { GOOGLE_DRIVE_RESUME_URL } from "$env/static/private"
 
-export const load = () => {
+export const GET = () => {
     redirect(302, GOOGLE_DRIVE_RESUME_URL)
 }
