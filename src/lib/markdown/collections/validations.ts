37e2e8f05f4c3a48d3f8efcb124f9f4b39cc404a
babakfp <babak.bxf@gmail.com>
2024-10-28 16:23:41 +0330

    Moved var into func

--- src/lib/markdown/collections/validations.ts
+++ src/lib/markdown/collections/validations.ts
@@ -1,29 +1,29 @@
 import { error } from "@sveltejs/kit"
 import { z } from "zod"
 
-const collectionSchema = z
-    .string()
-    .min(1)
-    .regex(
-        /^[a-z]+$/,
-        "The collection name can only contain lowercase letters.",
-    )
-
-const slugSegmentSchema = z
-    .string()
-    .min(1)
-    .regex(
-        /^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/,
-        "The collection entry name can only contain letters, numbers, and a single hyphen in between them.",
-    )
-
 export const validateCollection = (name: string) => {
+    const collectionSchema = z
+        .string()
+        .min(1)
+        .regex(
+            /^[a-z]+$/,
+            "The collection name can only contain lowercase letters.",
+        )
+
     const result = collectionSchema.safeParse(name)
     if (result.success) return
     return error(400, result.error.message)
 }
 
 export const validateSlugSegment = (name: string) => {
+    const slugSegmentSchema = z
+        .string()
+        .min(1)
+        .regex(
+            /^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/,
+            "The collection entry name can only contain letters, numbers, and a single hyphen in between them.",
+        )
+
     const result = slugSegmentSchema.safeParse(name)
     if (result.success) return
     return error(400, result.error.message)
