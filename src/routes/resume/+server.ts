43a3ca995b03a65a268bfd97be928aedfdf5d41d
babakfp <babak.bxf@gmail.com>
2024-11-08 19:36:57 +0330

    Throw error if GOOGLE_DRIVE_RESUME_URL was not set

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -2,5 +2,8 @@ import { redirect } from "@sveltejs/kit"
 import { GOOGLE_DRIVE_RESUME_URL } from "$env/static/private"
 
 export const GET = () => {
+    if (!GOOGLE_DRIVE_RESUME_URL) {
+        throw new Error("GOOGLE_DRIVE_RESUME_URL is not set.")
+    }
     redirect(302, GOOGLE_DRIVE_RESUME_URL)
 }
