38b97ee2b09caee1e8274d035958cd17520b55fe
babakfp <babak.bxf@gmail.com>
2024-11-09 16:51:55 +0330

    Removed unneeded check

--- src/lib/constants/env.ts
+++ src/lib/constants/env.ts
@@ -1,9 +1,5 @@
 import { PUBLIC_GOOGLE_DRIVE_RESUME_URL } from "$env/static/public"
 
-if (!PUBLIC_GOOGLE_DRIVE_RESUME_URL) {
-    throw new Error("PUBLIC_GOOGLE_DRIVE_RESUME_URL is not set.")
-}
-
 export const env = {
     PUBLIC_GOOGLE_DRIVE_RESUME_URL,
 }
