39025423a4a8c7a07d42d44c06f3f281eb6927ad
babakfp <babak.bxf@gmail.com>
2025-03-01 20:57:02 +0330

    improve compoennt

--- src/lib/components/UtilsTable.svelte
+++ src/lib/components/UtilsTable.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-    import UtilsTableProperties from "$lib/components/UtilsTableProperties.svelte"
-
     let {
         utilities,
     }: {
-        // TODO: Bad type.
         utilities: object
     } = $props()
 
@@ -42,7 +39,7 @@
                             {#if typeof selectorAndStyles === "string"}
                                 {@html selectorAndStyles}
                             {:else}
-                                <UtilsTableProperties {selectorAndStyles} />
+                                {@render Properties({ selectorAndStyles })}
                             {/if}
                         </td>
                     </tr>
@@ -51,6 +48,62 @@
         </table>
     </div>
 </div>
+
+{#snippet Properties({
+    selectorAndStyles,
+    indent = $bindable(16),
+}: {
+    selectorAndStyles: object
+    indent?: number
+})}
+    <div class="leading-6">
+        {#each Object.entries(selectorAndStyles) as [propertyOrSelector, valueOrObjectOfPropertyAndValue]}
+            <!-- For @apply -->
+            {#if propertyOrSelector.includes("@apply")}
+                <span style="color: #908CAA">{"@apply"}</span>
+                <span style="color: #9CCFD8">
+                    {propertyOrSelector.replace("@apply", "").trim()}<span
+                        style="color: #908CAA">{";"}</span
+                    >
+                </span>
+            {:else if valueOrObjectOfPropertyAndValue === ""}
+                <!-- For variants -->
+                <!-- selector -->
+                <div>{propertyOrSelector}</div>
+            {:else if typeof valueOrObjectOfPropertyAndValue === "string"}
+                <!-- property: value -->
+                <div>
+                    <!-- prettier-ignore -->
+                    <span style="color: #9CCFD8">{propertyOrSelector}<span style="color: #908CAA">:</span></span>
+                    <!-- prettier-ignore -->
+                    <span style="color: {Number.isNaN(valueOrObjectOfPropertyAndValue) ? '#F6C177' : '#EA9A97'}">{valueOrObjectOfPropertyAndValue}<span style="color: #908CAA">;</span></span>
+                </div>
+            {:else if typeof valueOrObjectOfPropertyAndValue === "object"}
+                <!-- {} -->
+                <!-- Spliting every selector into a new line
+		Used span to keep selector and { in the same line -->
+                {@const selectors = propertyOrSelector.split(",")}
+                {#each selectors as selector, i}
+                    {@const isNotTheLastSelector = i < selectors.length - 1}
+                    <svelte:element
+                        this={isNotTheLastSelector ? "div" : "span"}
+                        style="color: #908CAA"
+                    >
+                        {selector}{isNotTheLastSelector ? "," : ""}
+                    </svelte:element>
+                {/each}
+                <span style="color: #908CAA">{"{"}</span>
+                <div style="text-indent: {indent}px">
+                    {@render Properties({
+                        selectorAndStyles: valueOrObjectOfPropertyAndValue,
+                        indent: indent + 16,
+                    })}
+                </div>
+                <span style="color: #908CAA">{"}"}</span>
+            {/if}
+        {/each}
+    </div>
+{/snippet}
 
 <!-- TODO -->
 <!-- prettier-ignore -->
