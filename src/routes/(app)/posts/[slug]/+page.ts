75e65fc10d79873e82db8e5a1ede2846d78456d4
babakfp <babak.bxf@gmail.com>
2025-09-05 00:10:44 +0330

    remove unneeded posts in page.ts of single post

--- src/routes/(app)/posts/[slug]/+page.ts
+++ src/routes/(app)/posts/[slug]/+page.ts
@@ -1,6 +1,5 @@
 import { error } from "@sveltejs/kit"
 import { collections } from "$lib/markdown/collections.js"
-import { getPosts } from "$lib/utilities/getPosts.js"
 
 export const load = async ({ params }) => {
     const post = collections.getEntry("posts", params.slug)
@@ -9,8 +8,5 @@ export const load = async ({ params }) => {
         error(404)
     }
 
-    return {
-        ...post,
-        posts: await getPosts(),
-    }
+    return post
 }
