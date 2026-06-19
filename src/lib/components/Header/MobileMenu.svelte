9f76a3f209e5b1d4140959f440e91494c12a3713
babakfp <babak.bxf@gmail.com>
2026-06-19 21:53:40 +0330

    simple

--- src/lib/components/Header/MobileMenu.svelte
+++ src/lib/components/Header/MobileMenu.svelte
@@ -15,11 +15,7 @@
                     class="outline-inset items-center group-last:pr-4 group-first:pl-4 flex px-2"
                     href={item.href}
                 >
-                    <div
-                        class="highlight-gray-50/10 flex h-9 w-9 items-center justify-center rounded-lg bg-gray-50/5 text-gray-300 shadow-sm duration-100 group-hover:bg-gray-50/10 group-hover:text-gray-50"
-                    >
-                        <item.icon class="text-xl" />
-                    </div>
+                    <item.icon class="text-2xl" />
                 </a>
             </li>
         {/each}
