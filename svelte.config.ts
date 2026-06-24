4d3f3a4bc91f36044a46417fb9e8421408b1895c
babakfp <babak.bxf@gmail.com>
2026-06-24 21:22:07 +0330

    /

--- svelte.config.ts
+++ svelte.config.ts
@@ -15,30 +15,23 @@ export default {
             elements: ["blockquote", "img", "pre"],
             onTransform: (options, config) => {
                 return unifiedTransformer(options, config, {
-                    builtInPlugins: {
-                        remarkToc: {
-                            enable: false,
-                        },
-                        rehypeAutolinkHeadings: {
-                            enable: true,
-                            options: {
-                                behavior: "append",
-                                properties: {
-                                    class: "heading-permalink",
-                                    "aria-label": "Permalink to this headline",
-                                },
-                                content: () => {
-                                    return hastFromHtml(
-                                        '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M216 152h-48v-48h48a8 8 0 0 0 0-16h-48V40a8 8 0 0 0-16 0v48h-48V40a8 8 0 0 0-16 0v48H40a8 8 0 0 0 0 16h48v48H40a8 8 0 0 0 0 16h48v48a8 8 0 0 0 16 0v-48h48v48a8 8 0 0 0 16 0v-48h48a8 8 0 0 0 0-16Zm-112 0v-48h48v48Z"/></svg>',
-                                    )
-                                },
-                                test: ["h2", "h3", "h4", "h5", "h6"],
+                    remarkToc: {
+                        enable: false,
+                    },
+                    rehypeAutolinkHeadings: {
+                        enable: true,
+                        options: {
+                            behavior: "append",
+                            properties: {
+                                class: "heading-permalink",
+                                "aria-label": "Permalink to this headline",
                             },
-                        },
-                        rehypeShiki: {
-                            options: {
-                                theme: "slack-dark",
+                            content: () => {
+                                return hastFromHtml(
+                                    '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M216 152h-48v-48h48a8 8 0 0 0 0-16h-48V40a8 8 0 0 0-16 0v48h-48V40a8 8 0 0 0-16 0v48H40a8 8 0 0 0 0 16h48v48H40a8 8 0 0 0 0 16h48v48a8 8 0 0 0 16 0v-48h48v48a8 8 0 0 0 16 0v-48h48a8 8 0 0 0 0-16Zm-112 0v-48h48v48Z"/></svg>',
+                                )
                             },
+                            test: ["h2", "h3", "h4", "h5", "h6"],
                         },
                     },
                 })
