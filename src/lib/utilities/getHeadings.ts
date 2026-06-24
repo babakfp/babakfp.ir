669dfbfc891570350ad5c9140b6c7d3aba1cd733
babakfp <babak.bxf@gmail.com>
2026-06-24 10:31:09 +0330

    rm unneeded code

--- src/lib/utilities/getHeadings.ts
+++ src/lib/utilities/getHeadings.ts
@@ -9,7 +9,6 @@ export type Headings = {
 export const getHeadings = () =>
     makeToc({
         container: ".article-content",
-        headingLevels: [2, 3, 4, 5, 6],
     }).map((heading) => ({
         id: heading.attributes.id ?? "",
         level: Number(heading.level),
