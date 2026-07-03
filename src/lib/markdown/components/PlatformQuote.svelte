f86709a1edd19efd9c47db02418047118aa14bb0
babakfp <babak.bxf@gmail.com>
2026-07-03 22:36:11 +0330

    style changes

--- src/lib/markdown/components/PlatformQuote.svelte
+++ src/lib/markdown/components/PlatformQuote.svelte
@@ -1,11 +1,5 @@
-<script module lang="ts">
-    // 1. Create a unique key for the nesting context
-    const NESTING_KEY = "nesting-index" // Symbol("nesting-index")
-</script>
-
 <script lang="ts">
     import type { Snippet } from "svelte"
-    import { getContext, setContext } from "svelte"
 
     let {
         title,
@@ -14,26 +8,10 @@
         title: string
         children: Snippet
     } = $props()
-
-    // 2. Look for a parent's index. If none exists, start at -1.
-    const parentIndex = getContext<number | undefined>(NESTING_KEY) ?? -1
-
-    // 3. Increment the index for the current instance (First instance = 0)
-    const index = parentIndex + 1
-
-    // 4. Share this instance's index with any deeply nested children
-    setContext(NESTING_KEY, index)
 </script>
 
-<div
-    class="platform-quote relative overflow-hidden rounded-lg p-4 sm:p-8 ring"
-    style:--tw-ring-color={`oklch(from var(--color-orange-400) l c calc(h + ${index} * 70) / 10%)`}
-    style:background-color={`oklch(from var(--color-orange-400) l c calc(h + ${index} * 70) / 5%)`}
->
-    <div
-        class="font-extralight text-2xl"
-        style:color={`oklch(from var(--color-orange-400) l c calc(h + ${index} * 70) / 50%)`}
-    >
+<div class="platformquote relative overflow-hidden rounded-lg p-4 sm:p-8 ring">
+    <div class="font-extralight text-2xl">
         {title}
     </div>
 
