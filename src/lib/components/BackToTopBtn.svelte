cf8bfa238910afa032d6732a1b7083d1b0e72b29
babakfp <babak.bxf@gmail.com>
2026-08-04 02:45:25 +0330

    better

--- src/lib/components/BackToTopBtn.svelte
+++ src/lib/components/BackToTopBtn.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import IconArrowLineUpRegular from "phosphor-icons-svelte/IconArrowLineUpRegular.svelte"
     import { innerHeight, scrollY } from "svelte/reactivity/window"
+
+    const showButton = $derived(
+        (scrollY.current ?? 0) >= (innerHeight.current ?? 0) * 2,
+    )
 </script>
 
 <a
     class={[
-        "btn fixed right-4 bottom-0 z-30 translate-y-full duration-200",
-        { "-translate-y-4!": scrollY >= innerHeight },
+        "btn hide fixed right-4 bottom-4 z-30 translate-y-4 transition-all duration-200",
+        { "show! translate-y-0!": showButton },
     ]}
     href="#body"
     aria-label="Return to top"
